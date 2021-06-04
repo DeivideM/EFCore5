9dde556faeb4c18b003c3aa4b99bcf4de201562d
Deivide Martins <deivide_martins@hotmail.com>
2021-06-04 05:39:10 -0400

    Refactored Entities Created ModelBuilderExtension Created PersonEntityTypeConfiguration

---
+++ EFCore5.DAL/Migrations/20210604093422_Initial.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5.DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "LookUps",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LookUpType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LookUps", x => x.Code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Person_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    EmailAddress = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Person_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddressLine1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalSchema: "dbo",
+                        principalTable: "Persons",
+                        principalColumn: "Person_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "LookUps",
+                columns: new[] { "Code", "Description", "LookUpType" },
+                values: new object[,]
+                {
+                    { "AL", "Alabama", 0 },
+                    { "NJ", "New Jersey", 0 },
+                    { "NM", "New Mexico", 0 },
+                    { "NY", "New York", 0 },
+                    { "NC", "New Carolina", 0 },
+                    { "ND", "North Dakota", 0 },
+                    { "OH", "Ohio", 0 },
+                    { "OK", "Oklahoma", 0 },
+                    { "OR", "Oregon", 0 },
+                    { "PA", "Pennsylvania", 0 },
+                    { "RI", "Rhode Island", 0 },
+                    { "SC", "South Carolina", 0 },
+                    { "SD", "South Dakota", 0 },
+                    { "TN", "Tennessee", 0 },
+                    { "TX", "Texas", 0 },
+                    { "UT", "Utah", 0 },
+                    { "VT", "Vermont", 0 },
+                    { "VA", "Virginia", 0 },
+                    { "WA", "Washington", 0 },
+                    { "WV", "West Virginia", 0 },
+                    { "WI", "Wisconsis", 0 },
+                    { "WY", "Wyoming", 0 },
+                    { "PR", "Puerto Rico", 0 },
+                    { "USA", "United States of America", 1 },
+                    { "NH", "New Hampshire", 0 },
+                    { "NV", "Nevada", 0 },
+                    { "NE", "Montana", 0 },
+                    { "MT", "Missouri", 0 },
+                    { "AK", "Alaska", 0 },
+                    { "AZ", "Arizona", 0 },
+                    { "AR", "Arkansas", 0 },
+                    { "CA", "California", 0 },
+                    { "CO", "Colorado", 0 },
+                    { "CT", "Connecticut", 0 },
+                    { "DE", "Delaware", 0 },
+                    { "DC", "District of Columbia", 0 },
+                    { "FL", "Florida", 0 },
+                    { "GA", "Georgia", 0 },
+                    { "ID", "Hawaii", 0 },
+                    { "IL", "Idaho", 0 },
+                    { "IA", "Indiana", 0 },
+                    { "KS", "Iowa", 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "LookUps",
+                columns: new[] { "Code", "Description", "LookUpType" },
+                values: new object[,]
+                {
+                    { "KY", "Kansas", 0 },
+                    { "LA", "Kentucky", 0 },
+                    { "ME", "Louisiana", 0 },
+                    { "MD", "Maine", 0 },
+                    { "MA", "Maryland", 0 },
+                    { "MI", "Massachusetts", 0 },
+                    { "MN", "Michigan", 0 },
+                    { "MS", "Minnesota", 0 },
+                    { "MO", "Mississippi", 0 },
+                    { "IN", "Illinois", 0 }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Persons",
+                columns: new[] { "Person_Id", "EmailAddress", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "john@smith.com", "John", "Smith" },
+                    { 2, "susan@jones.com", "Susan", "Jones" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "AddressLine1", "AddressLine2", "City", "Country", "CreatedOn", "PersonId", "State", "ZipCode" },
+                values: new object[] { 1, "123 Test St", "", "Beverly Hills", "USA", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "CA", "90210" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "AddressLine1", "AddressLine2", "City", "Country", "CreatedOn", "PersonId", "State", "ZipCode" },
+                values: new object[] { 2, "123 Michigan Ave", "", "Chicago", "USA", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "IL", "60612" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "AddressLine1", "AddressLine2", "City", "Country", "CreatedOn", "PersonId", "State", "ZipCode" },
+                values: new object[] { 3, "100 1St St", "", "Chicago", "USA", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "IL", "60612" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_PersonId",
+                table: "Addresses",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "LookUps");
+
+            migrationBuilder.DropTable(
+                name: "Persons",
+                schema: "dbo");
+        }
+    }
+}
---
+++ EFCore5.DAL/PersonEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using EFCore5.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore5.DAL
+{
+    public class PersonEntityTypeConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder
+                .ToTable("Persons", "dbo")
+                .Ignore(p => p.FullName);
+
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Person_Id");
+
+            builder
+                .Property(p => p.FirstName)
+                .HasColumnType("varchar(250)")
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder
+                .Property(p => p.LastName)
+                .HasColumnType("varchar(250)")
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder
+              .Property(p => p.EmailAddress)
+              .HasColumnType("varchar(500)")
+              .HasMaxLength(500)
+              .IsRequired();
+
+            builder
+              .Property(p => p.CreatedOn)
+              .HasDefaultValueSql("getdate()");
+
+        }
+    }
+}
